673808674e709998b0a529f43a3353adcbf2281e
Eldon Kuzhyelil <eldon.mec@gmail.com>
2019-12-16 13:33:07 +0100

    Autodeployment enabled testing

--- index.js
+++ index.js
@@ -14,55 +14,61 @@ app.use(
 
 // First route
 app.get('/', (req, res) => {
-    res.json({ message: 'Send POST with JSON format:{"address":{"colorKeys":["A","G","Z"],"values":[-20,4]},"meta":{"digits":33,"processingPattern":"d{5}+[a-z&$§]"}} '})
+    res.json({ message: 'AutoDeployment enabled! Send POST with JSON format:{"address":{"colorKeys":["A","G","Z"],"values":[-20,4]},"meta":{"digits":33,"processingPattern":"d{5}+[a-z&$§]"}} '})
 })
 
 app.post('/', (req, res) => {
-		isFailed = false
-       try {        
-            console.log(req.body.address.values[0]);
-            console.log('JSON Parsing function...');
-            //JSON.parse(req.body);
-      
-        } 
-        catch (err) { 
-            //console.log("Error : " + err);
-            console.log('Wrong JSON format');
-            res.status(404).send({'result':'Input not in the expected JSON format'});
-            isFailed = true
-        }
-		if(!isFailed){
+	isFailed = false
+	try {        
+		console.log(req.body.address.values[0]);
+		console.log('JSON Parsing function...');
+		//JSON.parse(req.body);
 	
-			console.log('OK continue');
-			var valueSum = 0;
-			for(var i = 0; i < req.body.address.values.length; i++) {
-				//console.log (req.body.address.values[i])
-				value = req.body.address.values[i]
-				try{
-				   if( !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value, 10))){                   
-						valueSum+=value
-				   }
-				   else{
-					   err2 = new error('Values node contain non numbers')
-				   }
+	} 
+	catch (err) { 
+		//console.log("Error : " + err);
+		console.log('Wrong JSON format');
+		res.status(404).send({'result':'Input not in the expected JSON format'});
+		isFailed = true
+	}
+	if(!isFailed){
+		var valueSum = 0;
+		for(var i = 0; i < req.body.address.values.length; i++) {
+			//console.log (req.body.address.values[i])
+			
+			try{
+				value =  req.body.address.values[i]
+				validValueToAdd =  parseInt(req.body.address.values[i])
+				if( !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value, 10))){                   
+					console.log(validValueToAdd)
+					valueSum+=validValueToAdd
 				}
-				catch(err2){
-					console.log("Error : " + err2);
-					console.log('non numbers format');
-					res.status(404).send({'result':'Values node contains non numbers'});
+				else{
+					err2 = new error('Values node contain non numbers')
 				}
-
-			} 
-			valueSum = Math.abs(valueSum) //neglecting negative sign
-			digitSum = 0;
-			while (valueSum) {       
-				digitSum += valueSum % 10;
-				valueSum = Math.floor(valueSum / 10);
 			}
-
-			res.json({'result':digitSum})
+			catch(err2){
+				console.log("Error : " + err2);
+				console.log('non numbers format');
+				res.status(404).send({'result':'Values node contains non numbers'});
+			}
 		}
+		digitSum = calculateDigitSum(valueSum)
+		res.json({'result':digitSum})
+	}
 })
 
+
+function calculateDigitSum(valueSum) { 
+	console.log('OK continue')
+	 
+	valueSum = Math.abs(valueSum) //neglecting negative sign
+	digitSum = 0;
+	while (valueSum) {       
+		digitSum += valueSum % 10;
+		valueSum = Math.floor(valueSum / 10);
+	}
+	return digitSum;
+ }
 // Starting server
 app.listen(process.env.PORT || 5000)
--- test/test.js
+++ test/test.js
@@ -31,19 +31,27 @@ describe("Unit tests",function(){
 
 
   it("should return expected result and status 200 - POST",function(done){
-
+    num1 = rand(1,999) 
+    num2  = rand(1,9999) 
+    valueSum = Math.abs(num1 + num2) //neglecting negative sign
+    expectedDigitSum = 0;
+    while (valueSum) {       
+      expectedDigitSum += valueSum % 10;
+      valueSum = Math.floor(valueSum / 10);
+    }
+    console.log('expectedDigitSum:')
+    console.log(expectedDigitSum)
     //calling POST api
     server
     .post('/')
-    .send({"address":{"colorKeys":["A","G","Z"],"values":[8,50]},"meta":{"digits":33,"processingPattern":"d{5}+[a-z&$§]"}})
+    .send({"address":{"colorKeys":["A","G","Z"],"values":[num1,num2]},"meta":{"digits":33,"processingPattern":"d{5}+[a-z&$§]"}})
     .expect("Content-type",/json/)
     .expect(200)
     .end(function(err,res){
-      console.log("res.body.result")
+      console.log("res.body.result:")
       console.log(res.body.result)
       res.status.should.equal(200);
-      //res.body.error.should.equal(false);
-      res.body.result.should.equal(13); //8+50 = 58 -> 5+8=13
+      res.body.result.should.equal(expectedDigitSum); //8+50 = 58 -> 5+8=13
       done();
     });
   });
@@ -58,8 +66,7 @@ describe("Unit tests",function(){
     .expect(404)
     .end(function(err,res){
       res.status.should.equal(404);
-      //res.body.error.should.equal(false);
-     // res.body.result.should.equal(13);
+      res.body.result.should.equal("Input not in the expected JSON format"); 
       done();
     });
   });
@@ -74,10 +81,18 @@ describe("Unit tests",function(){
     .expect(404)
     .end(function(err,res){
       res.status.should.equal(404);
-      //res.body.error.should.equal(false);
-     // res.body.result.should.equal(13);
+      res.body.result.should.equal("Values node contains non numbers"); 
       done();
     });
   });
 
 });
+rand = function(min, max) {
+  if (min==null && max==null)
+    return 0;  
+  if (max == null) {
+      max = min;
+      min = 0;
+  }
+  return min + Math.floor(Math.random() * (max - min + 1));
+};
